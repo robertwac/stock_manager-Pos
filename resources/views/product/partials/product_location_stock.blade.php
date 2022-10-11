a9af0e0fc2c015d2a60e1bc6bb07386d0aad86ce
robertwac <robertkimaru1998@gmail.com>
2022-10-11 10:25:04 +0300

    initial commit

---
+++ resources/views/product/partials/product_location_stock.blade.php
@@ -0,0 +1,37 @@
+<style>
+    #all_stock_report {
+  width: 100% !important;
+}
+</style>
+            <table class="table table-bordered table-striped" id="all_stock_report" style="width: 100% !important;">
+                <thead class="w-100" style="width: 100% !important;">
+                    <tr>
+                        <th>@lang('sku')</th>
+                        <th>@lang('name')</th>
+                        <th>@lang('BL0001')</th>
+                        <th>@lang('BL0002')</th>
+                        <th>@lang('BL0003')</th>
+                        <th>@lang('BL0004')</th>
+                        <th>@lang('BL0005')</th>
+                        <th>@lang('BL0006')</th>
+                        <th>@lang('Brand')</th>
+                        <th>@lang('Category')</th>
+                    </tr>
+                </thead>
+               
+                <tfoot>
+                    <tr>
+                        <th>@lang('sku')</th>
+                        <th>@lang('name')</th>
+                        <th>@lang('BL0001')</th>
+                        <th>@lang('BL0002')</th>
+                        <th>@lang('BL0003')</th>
+                        <th>@lang('BL0004')</th>
+                        <th>@lang('BL0005')</th>
+                        <th>@lang('BL0006')</th>
+                        <th>@lang('Brand')</th>
+                       <th>@lang('Category')</th>
+                    </tr>
+                </tfoot>
+            </table>
+  
